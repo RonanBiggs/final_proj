b02742d51fbaccc6ae2f6972e979567f73e05cf2
Ryan Schmitt <ryan15schmitt@gmail.com>
2026-03-13 16:58:56 -0700

    Added main class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package tsp;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> new TspFrame().setVisible(true));
+  }
+}
